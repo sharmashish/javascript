6b8351c442a5d1f8114f0e805caf0a16ddd52256
Ashish Sharma <51817883+sharmashish@users.noreply.github.com>
2023-12-12 09:20:07 +0000

    conversion of dataTypes

---
+++ 01-Basics/03_conversion.js
@@ -0,0 +1,19 @@
+let score = "33ashish";
+
+
+// console.log(typeof score);
+
+let ValueInNumber = Number(score);
+// console.log(typeof(ValueInNumber));
+// console.log(ValueInNumber);
+
+let isLoggedIn = 0;
+let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof(booleanLoggedIn));
+// console.log(booleanLoggedIn);
+
+
+let num1 = 223;
+let stringNum = String(num1)
+console.log(stringNum);
+console.log(typeof(stringNum));
