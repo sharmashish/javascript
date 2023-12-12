926e7667e477af8bf297624ece2ebe64ff25be35
Ashish Sharma <51817883+sharmashish@users.noreply.github.com>
2023-12-12 10:16:07 +0000

    comparision

--- 01-Basics/03_conversion.js
+++ 01-Basics/03_conversion.js
@@ -15,5 +15,29 @@ let booleanLoggedIn = Boolean(isLoggedIn);
 
 let num1 = 223;
 let stringNum = String(num1)
-console.log(stringNum);
-console.log(typeof(stringNum));
+// console.log(stringNum);
+// console.log(typeof(stringNum));
+
+
+//  OPERATIONS
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+
+
+let str1 =  "Hello";
+let str2 = " Ashish"
+let str3 = str1 + str2;
+// console.log((str3));
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+  
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter)
