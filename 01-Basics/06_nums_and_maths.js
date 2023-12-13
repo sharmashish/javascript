4d51c5ce764e68945e4c9ef54db77b796d8f65ce
Ashish Sharma <51817883+sharmashish@users.noreply.github.com>
2023-12-13 10:42:05 +0000

    numAndMaths

---
+++ 01-Basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 400;
+// console.log(score,"score");
+
+const balance = new Number(100);
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+
+
+
+
+
+
+// ----------------------------     MATHS      ------------------------------------------
+
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(44.7));
+// console.log(Math.floor(32.3));
+// console.log(Math.ceil(33.56));
+
+
+console.log(Math.random());
+console.log((Math.random()* 10)+1);
+
+const min = 10;
+const max = 20;
+
+
+console.log(Math.floor((Math.random() * (max-min +1)) +min))
