157d579cf48dcddfb81a02889bee3f6c76a22932
Ashish Sharma <51817883+sharmashish@users.noreply.github.com>
2023-12-13 09:49:48 +0000

    string

---
+++ 01-Basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Ashish"
+const repoCount = 10
+
+// console.log(name + repoCount)
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Sharma")
+
+console.log(gameName[0])
+console.log(gameName.__proto__);
+console.log(gameName.length);
